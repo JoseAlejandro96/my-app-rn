e8f158090eff1e2e89f483d6cd9c712f18d065a8
Jose Alejandro <jscuesta96@gmail.com>
2022-07-19 23:15:42 +0200

    feat() login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,10 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import React from 'react';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -6,7 +12,11 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import LoginSVG from '../../assets/images/login.svg';
 import { TextInput } from 'react-native-gesture-handler';
 
-const Login = () => {
+import GoogleSVG from '../../assets/images/google.svg';
+import FacebookSVG from '../../assets/images/facebook.svg';
+import TwitterSVG from '../../assets/images/twitter.svg';
+
+const Login = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
       <View style={{ padding: 25 }}>
@@ -70,9 +80,81 @@ const Login = () => {
             <Text style={{ color: '#AD40AF', fontWeight: '700' }}>Forgot?</Text>
           </TouchableOpacity>
         </View>
+
+        <TouchableOpacity onPress={() => {}} style={styles.loginButtom}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <Text style={{ textAlign: 'center', color: '#666', marginBottom: 30 }}>
+          Or, login with ...
+        </Text>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 30,
+          }}
+        >
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <GoogleSVG height={24} width={24} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <FacebookSVG height={24} width={24} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <TwitterSVG height={24} width={24} />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 30,
+          }}
+        >
+          <Text>New to the app?</Text>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Register');
+            }}
+          >
+            <Text
+              style={{ color: '#AD40AF', fontWeight: '700', marginLeft: 5 }}
+            >
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  loginButtom: {
+    backgroundColor: '#AD40AF',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#fff',
+  },
+  footerButton: {
+    borderColor: '#ddd',
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
+});
 
 export default Login;
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,12 +1,156 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import React from 'react';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import RegistrationSVG from '../../assets/images/registration.svg';
+import { TextInput } from 'react-native-gesture-handler';
+
+import GoogleSVG from '../../assets/images/google.svg';
+import FacebookSVG from '../../assets/images/facebook.svg';
+import TwitterSVG from '../../assets/images/twitter.svg';
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{ padding: 25 }}>
+        <View style={{ alignItems: 'center' }}>
+          <RegistrationSVG
+            height={300}
+            width={300}
+            style={{ transform: [{ rotate: '-5deg' }] }}
+          />
+        </View>
+        <Text style={{ fontSize: 28, fontWeight: '500', color: '#333' }}>
+          Register
+        </Text>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <MaterialIcons
+            name='alternate-email'
+            size={20}
+            color={'#666'}
+            style={{ marginRight: 5 }}
+          />
+          <TextInput
+            placeholder='Email ID'
+            style={{ flex: 1, paddingVertical: 0 }}
+            keyboardType={'email-address'}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <Ionicons
+            name='ios-lock-closed-outline'
+            size={20}
+            color={'#666'}
+            style={{ marginRight: 5 }}
+          />
+          <TextInput
+            placeholder='Password'
+            style={{ flex: 1, paddingVertical: 0 }}
+            secureTextEntry={true}
+          />
+
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={{ color: '#AD40AF', fontWeight: '700' }}>Forgot?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity onPress={() => {}} style={styles.loginButtom}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <Text style={{ textAlign: 'center', color: '#666', marginBottom: 30 }}>
+          Or, login with ...
+        </Text>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 30,
+          }}
+        >
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <GoogleSVG height={24} width={24} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <FacebookSVG height={24} width={24} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}} style={styles.footerButton}>
+            <TwitterSVG height={24} width={24} />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginBottom: 30,
+          }}
+        >
+          <Text>New to the app?</Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text
+              style={{ color: '#AD40AF', fontWeight: '700', marginLeft: 5 }}
+            >
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  loginButtom: {
+    backgroundColor: '#AD40AF',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#fff',
+  },
+  footerButton: {
+    borderColor: '#ddd',
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
+});
 
 export default Register;
